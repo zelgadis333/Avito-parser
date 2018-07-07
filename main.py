1a734d26a2d82b44c0931b2b052a15419e560de0
zelgadis333 <33630368+zelgadis333@users.noreply.github.com>
2018-07-08 02:48:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import csv
+import time
 
-def load_user_data(page, session):
-    url = 'https://www.avito.ru/moskva/noutbuki?p=%d' % (page)
-    # print url
+def load_user_data(page, session, url):
+
     request = session.get(url)
     return request.text
+
 
 def read_file(filename):
     with open(filename) as input_file:
@@ -21,8 +22,9 @@ def parse_user_datafile_bs(filename):
 
     soup = BeautifulSoup(text, "html.parser")
     item_list = soup.find_all('div', {'class': 'item_table-header'})
+    i = 0
     for item in item_list:
-
+        
         item_link = item.find('h3', {'class': 'item-description-title'}).find('a').get('href')
         item_title = item.find('h3', {'class': 'item-description-title'}).find('a').get('title')
         try:
@@ -42,16 +44,17 @@ def parse_user_datafile_bs(filename):
             item_description = ""
             item_date_add = ""
         try:
-            item_adress = soup.find_all('div', {'class': 'seller-info-prop'})[4].find('div', {'class': 'seller-info-value'}).text
+            item_address = soup.find_all('div', {'class': 'seller-info-prop'})[4].find('div', {'class': 'seller-info-value'}).text
         except:
-            item_adress = ""
+            item_address = ""
         try:
             item_owner = soup.find('div', {'class': 'seller-info js-seller-info'}).find('a').text
         except:
             item_owner = ""
-            resultsanswer.append([item_link, item_title, item_price, item_date_add, avito_id, item_description, item_adress, item_owner])
+        resultsanswer.append([item_link, item_title, item_price, item_date_add, avito_id, item_description,
+                                  item_address, item_owner])
+        time.sleep(5)
     return resultsanswer
-
 
 
 s = requests.Session()
@@ -62,9 +65,9 @@ s.headers.update({
 
 
 page = 1
-while page < 2:
+while page < 10:
     # print 'Loading page #%d' % (page)
-    data = load_user_data(page, s)
+    data = load_user_data(page, s,  'https://www.avito.ru/moskva/noutbuki?p=%d' % page)
 
     with open('./user_data/page_%d.html' % page, 'w', encoding='utf8') as output_file:
         output_file.write(data)
@@ -76,9 +79,11 @@ for filename in os.listdir('./user_data/'):
     results.extend(parse_user_datafile_bs('./user_data/' + filename))
 
 fnames = ['item_link', 'item_title', 'item_price', 'item_date_add', 'avito_id', 'item_description', 'item_adress', 'item_owner']
-f = open('numbers3.csv', 'w')
+f = open('numbers3.csv', 'w', encoding='utf-16')
 writer = csv.DictWriter(f, fieldnames=fnames)
 writer.writeheader()
 for i in range(0, len(results)):
-    writer.writerow({'item_link': results[i][0], 'item_title': results[i][1], 'item_price': results[i][2], 'avito_id': results[i][4]})
+    writer.writerow({'item_link': results[i][0], 'item_title': results[i][1], 'item_price': results[i][2],
+                     'item_date_add': results[i][3], 'avito_id': results[i][4], 'item_description': results[i][5],
+                     'item_adress': results[i][6], 'item_owner': results[i][7]})
 f.close()
